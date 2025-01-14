c29407f5b9a1373345f544e8fe65b5f743796854
Nicolly Genuca <142937580+NicollyGenuca@users.noreply.github.com>
2025-01-14 17:47:01 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function Home() {
+    return (
+      <h2
+        style={{
+          background: 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          fontSize: '2rem',
+          textAlign: 'center',
+          fontWeight: 'bold',
+        }}
+      >
+        eu amo você
+      </h2>
+    );
+  }
+  
+  export default Home;
+  
